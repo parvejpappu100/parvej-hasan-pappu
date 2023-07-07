9e6af9392b04bf38091222030a55db64417ecaf9
parvejpappu-100 <parvejhasanpappu85@gmail.com>
2023-07-07 20:07:18 +0600

    added work exprerience

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,12 +3,12 @@ import SectionTitle from '../SectionTitle/SectionTitle';
 
 const About = () => {
     return (
-        <div className='my-32'>
+        <div className='py-28 max-w-7xl mx-auto'>
             <SectionTitle
                 title={"About Me"}
                 subTitle={"Know Me More"}
             ></SectionTitle>
-            <div className='flex lg:items-center flex-col lg:flex-row px-2 gap-8 mt-20 max-w-7xl mx-auto'>
+            <div className='flex lg:items-center flex-col lg:flex-row px-2 gap-8 mt-20 '>
                 <div className='lg:w-3/5'>
                     <h3 className='text-3xl font-bold'>I'm <span className='text-[#20C99A]'>Parvej Hasan Pappu,</span> a Web Developer</h3>
                     <p className='my-5 text-[18px]'>Hello, I'm a versatile MERN stack developer with a passion for crafting extraordinary web experiences. I excel in frontend and backend development, creating captivating user interfaces and robust server-side functionality. With expertise in HTML, CSS, and JavaScript, I build visually stunning and responsive websites. Let's collaborate to bring your vision to life and make an impact in the digital realm.</p>
@@ -20,6 +20,24 @@ const About = () => {
                     <p className='border-b mt-2 pb-2'><strong>Age :</strong> 20</p>
                     <p className='border-b mt-2 pb-2'><strong>From :</strong> Dhaka , Bangladesh</p>
                     <button className='bg-[#20C997] text-white font-semibold px-10 mt-5 hover:bg-[#1BAA80] duration-500 hover:shadow py-2 rounded-full'>Hire Me</button>
+                </div>
+            </div>
+            <div className='grid grid-cols-2 lg:grid-cols-4 gap-5 mt-16 text-center'>
+                <div className=' border-r-2 border-[#E0DEDE]'>
+                    <h1 className='text-5xl text-[#6C757D] font-semibold'>1+</h1>
+                    <p className='text-xl'>Years Experience</p>
+                </div>
+                <div className='lg:border-r-2 border-[#E0DEDE]'>
+                    <h1 className='text-5xl text-[#6C757D] font-semibold'>36+</h1>
+                    <p className='text-xl'>Happy Clients</p>
+                </div>
+                <div className='border-r-2 border-[#E0DEDE]'>
+                    <h1 className='text-5xl text-[#6C757D] font-semibold'>50+</h1>
+                    <p className='text-xl'>Projects Done</p>
+                </div>
+                <div className=''>
+                    <h1 className='text-5xl text-[#6C757D] font-semibold'>24</h1>
+                    <p className='text-xl'>Get Awards</p>
                 </div>
             </div>
         </div>
